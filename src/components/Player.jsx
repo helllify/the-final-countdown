e36ae6a8eeaefb3978c34d4fce7b0661516c5713
helllify <hellify.io@icloud.com>
2024-05-14 06:59:10 +0300

    forwarding refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -14,10 +14,7 @@ export default function Player() {
     <section id="player">
       <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input
-          ref={playerName}
-          type="text"
-        />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,16 +1,20 @@
-export default function ResultModal({ result, targetTime }) {
-    return (
-        <dialog className="result-modal" open>
-            <h2>You {result}</h2>
-            <p>
-                The target time was <strong>{targetTime} seconds.</strong>
-            </p>
-            <p>
-                You stopped the timer with <strong>X seconds left.</strong>
-            </p>
-            <form method="dialog">
-                <button>Close</button>
-            </form>
-        </dialog>
-    );
-}
+import { forwardRef } from 'react';
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+  return (
+    <dialog ref={ref} className="result-modal">
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+})
+
+export default ResultModal;
